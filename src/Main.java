725a115f52d4d2a5d5ace6eb2fde8524a4f6e806
OlgaMikhaylyukova <olele@inbox.ru>
2021-08-13 17:29:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 9999;
+        int rubles = 20;
+        int miles = price / rubles;
+                System.out.println("Amount of miles: " + miles);
+    }
+}
